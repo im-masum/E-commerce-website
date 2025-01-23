b277abc5d1fd0cfbda4f7d56319e008395b9235d
MIchowdhury <masum.iqbal.chowdhury@gmail.com>
2025-01-23 17:43:41 +0600

    Updating

--- script.js
+++ script.js
@@ -59,11 +59,11 @@ const products = [
   },
   {
     id: 8,
-    name: "Gaming Console",
+    name: "Smart Watch",
     price: 299.99,
     image:
-      "https://images.unsplash.com/photo-1486401899868-0e435ed85128?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-    category: "Gaming",
+      "https://images.unsplash.com/photo-1461141346587-763ab02bced9?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Electronics",
   },
 
   {
